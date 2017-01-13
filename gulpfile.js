04626f6e0ded4f65e347d081f65aa8ba72160463
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-13 04:52:26 +0300

    sepcify output name of file

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ elixir(function(mix) {
     var env = argv.e || argv.env || 'local';
     var port = argv.p || argv.port || 3000;
 
-    mix.sass('foundation.scss')
+    mix.sass(["app.scss"], "resources/assets/css/foundation.css")
         .exec('./vendor/bin/jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
             port: port,
