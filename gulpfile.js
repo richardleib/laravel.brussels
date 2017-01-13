f17857c28732ea7f2abeebe956328ec28c282ac4
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-13 16:17:09 +0300

    Tweak the gulpfile to get a result

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,12 @@ elixir.config.assetsPath = 'source/_assets';
 elixir.config.publicPath = 'source';
 
 elixir(function(mix) {
-    var env = argv.e || argv.env || 'local';
+    var env = argv.e || argv.env || 'production';
     var port = argv.p || argv.port || 3000;
 
-    mix.sass(["app.scss"], "resources/assets/css/foundation.css")
+    mix.sass(["app.scss"], "resources/assets/css/foundation.css");
+    mix.copy("resources/assets/css/foundation.css", "build_local/css");
+    mix.copy("resources/assets/css/foundation.css", "build_production/css")
         .exec('./vendor/bin/jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
             port: port,
