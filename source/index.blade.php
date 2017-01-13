9213d6e8bdfe48c81d1a4dfb9b6a705a987563c4
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-13 16:16:11 +0300

    Add some structure to the page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,14 +1,19 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>About Laravel Brussels</h1>
 
-<h2>Our Meetup Events</h2>
-<p></p>
-<h2>Contact Us</h2>
-<h2>Call For Speakers</h2>
-<h2>Code of Conduct</h2>
-<h2>Sponsorship & Support</h2>
-<h2>Other PHP Meetup Groups in Belgium</h2>
+    <div class="row">
+        <div class="columns small-12">
+            <h1>About Laravel Brussels</h1>
+
+            <h2>Our Meetup Events</h2>
+            <p>Lorem</p>
+            <h2>Contact Us</h2>
+            <h2>Call For Speakers</h2>
+            <h2>Code of Conduct</h2>
+            <h2>Sponsorship & Support</h2>
+            <h2>Other PHP Meetup Groups in Belgium</h2>
+        </div>
+    </div>
 
 @endsection
