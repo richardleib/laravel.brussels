5debdefa56b86adc5e4635db72d818c0cbe3f747
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-13 16:16:27 +0300

    Add site variables

--- config.php
+++ config.php
@@ -2,4 +2,14 @@
 
 return [
     'production' => true,
+    'twitter' => '@laravelbrussels',
+    'meetup_url' => '//meetup.com/Laravel-Brussels',
+    'other_organisation' => [
+        'PHP Antwerp' => '//www.meetup.com/phpantwerp',
+        'PHP Brussels' => '//www.meetup.com/BrusselsPHP',
+        'PHP Gent' => '//www.meetup.com/phpgent',
+        'PHP Leuven' => '//www.meetup.com/PHP-Leuven-Web-Innovation-Group',
+        'PHP Limburg (BE)' => '//www.meetup.com/PHP-Limburg-BE',
+        'Laracon EU' => '//laracon.eu',
+        ],
 ];
