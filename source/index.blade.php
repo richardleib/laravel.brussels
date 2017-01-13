41f823abc2c93da3674713f6f21a47c2ae742b1f
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-13 03:24:51 +0300

    Move the markdown to a standard blade file

---
+++ source/index.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('body')
+<h1>About Laravel Brussels</h1>
+
+<h2>Our Meetup Events</h2>
+<p></p>
+<h2>Contact Us</h2>
+<h2>Call For Speakers</h2>
+<h2>Code of Conduct</h2>
+<h2>Sponsorship & Support</h2>
+<h2>Other PHP Meetup Groups in Belgium</h2>
+
+@endsection
