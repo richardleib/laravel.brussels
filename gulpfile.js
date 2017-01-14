dea967e64ec53cd21e5a32d95f7e631133b34156
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-14 18:21:45 +0300

    Set paths correctly

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,14 @@
-var gulp = require('gulp');
-var elixir = require('laravel-elixir');
-var argv = require('yargs').argv;
+var gulp = require("gulp");
+var elixir = require("laravel-elixir");
+var argv = require("yargs").argv;
 
-elixir.config.assetsPath = 'source/_assets';
-elixir.config.publicPath = 'source';
+elixir.config.assetsPath = "source/_assets";
+elixir.config.publicPath = "source";
 
 elixir(function(mix) {
-    var env = argv.e || argv.env || 'local';
+    var env = argv.e || argv.env || "local";
     var port = argv.p || argv.port || 3000;
-
-    mix.sass(["app.scss"], "resources/assets/css/foundation.css");
-    mix.copy("resources/assets/css/foundation.css", "build_local/css");
-    mix.copy("resources/assets/css/foundation.css", "build_production/css")
+    mix.sass(["app.scss"], "source/css/foundation.css")
         .exec('./vendor/bin/jigsaw build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
         .browserSync({
             port: port,
