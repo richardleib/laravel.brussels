fdf97d3f1e376b67002e269c069e5faf4c22d24a
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-14 22:25:42 +0300

    Sponsorship section

--- source/index.blade.php
+++ source/index.blade.php
@@ -203,34 +203,18 @@
 
     </section>
 
-    <!-- events -->
-    <section>
+    <!-- sponsorship -->
+    <section class="sponsorship">
 
         <div class="row align-center">
             <div class="columns small-12 large-8 medium-9 content">
                 <h2>Sponsorship & Support</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate, orci sit amet tincidunt
-                    tincidunt, ex massa placerat est, quis vestibulum elit arcu a magna. Donec fermentum volutpat neque
-                    a commodo. Sed vehicula risus pharetra, pretium metus ut, dapibus dolor. Sed convallis molestie
-                    ultrices. Morbi at nulla neque. Integer commodo mattis ligula vel egestas. Lorem ipsum dolor sit
-                    amet, consectetur adipiscing elit. Etiam laoreet leo tortor, sed dignissim elit placerat a. Sed sit
-                    amet eros tempor lorem luctus aliquam sed sed mi. Quisque ut ante id nibh facilisis tempus eget ut
-                    ex. Morbi augue magna, scelerisque ac venenatis at, molestie non nibh. Phasellus sodales nisi neque,
-                    et luctus ex semper ut.
-                    Maecenas ultrices ipsum vitae lobortis fringilla.</p>
-
-                <p>Vivamus aliquet feugiat metus, sed vestibulum tellus molestie nec. Aenean purus nunc, pharetra ut
-                    quam nec, pretium mollis tellus. Nulla id eros lacus. Phasellus condimentum finibus nibh id mattis.
-                    Ut auctor tempor faucibus. Ut venenatis molestie elementum. Etiam in fermentum ex. Curabitur tellus
-                    urna, luctus in vestibulum in, lobortis vitae elit. Proin quis nisl ac velit gravida ornare. Fusce
-                    ut fermentum ex. Etiam at urna magna. Sed vestibulum, elit quis vehicula venenatis, ante odio
-                    finibus justo, in suscipit lacus ex et nulla. Vestibulum congue lacinia justo, sit amet rutrum augue
-                    elementum non. Nulla ornare nisl sollicitudin tortor sagittis luctus. Duis dictum pharetra nibh, eu
-                    tempus metus accumsan tincidunt.</p>
+                <p>The group is run by volunteers</p>
+                <p>We receive in-kind support from </p>
             </div>
         </div>
+
     </section>
-    <!-- events -->
 
     <!-- other-groups -->
     <section class="other-groups">
