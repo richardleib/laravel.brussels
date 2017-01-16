3a5d500a3f0d8049f12653023c963229ef9c1f27
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-16 16:35:04 +0300

    Title, description and dns-prefetching

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>@yield('title')</title>
+        <meta name="description" content="The Laravel Brussel Meetup is a group of PHP developers enthusiastic about
+        working with and sharing knowledge about the Laravel framework">
+        <link rel="dns-prefetch" href="//maxcdn.bootstrapcdn.com">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
