69c72d5ea8c6fdb563e00eec2656bbe800ae822f
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-16 16:34:26 +0300

    Make the nav a partial

---
+++ source/_nav.blade.php
@@ -0,0 +1,9 @@
+<img class="logo" title="Laravel Logo" alt="Laravel Logo" src="img/laravel.svg">
+<span class="social-icons">
+    <a href="{{ $meetup_url }}" title="Link to the Laravel Brussels group on Meetup.com>
+        <i class=" fa fa-3x fa-meetup" aria-hidden="true"></i>
+    </a>
+    <a href="//twitter.com/{{ $twitter }}" title="Link to our Twitter account">
+        <i class="fa fa-3x fa-twitter-square" aria-hidden="true"></i>
+    </a>
+</span>
