263d9c1bef8fe86e61aa6be656ac523bb6d49256
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-16 16:40:54 +0300

    Fix missing quote mark on the title

--- source/_nav.blade.php
+++ source/_nav.blade.php
@@ -1,6 +1,6 @@
 <img class="logo" title="Laravel Logo" alt="Laravel Logo" src="img/laravel.svg">
 <span class="social-icons">
-    <a href="{{ $meetup_url }}" title="Link to the Laravel Brussels group on Meetup.com>
+    <a href="{{ $meetup_url }}" title="Link to the Laravel Brussels group on Meetup.com">
         <i class=" fa fa-3x fa-meetup" aria-hidden="true"></i>
     </a>
     <a href="//twitter.com/{{ $twitter }}" title="Link to our Twitter account">
