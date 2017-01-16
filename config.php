6fb3b5dca21167dcd6045ec0edc281bece3d6e58
Richard Leibowitz <richardbbleibowitz@gmail.com>
2017-01-16 14:12:29 +0300

    Add PHP Benelux to other organisations

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ return [
     'meetup_url' => '//meetup.com/Laravel-Brussels',
     'other_organisations' => [
         'PHP Antwerp' => '//www.meetup.com/phpantwerp',
+        'PHP Benelux' => '//www.phpbenelux.eu',
         'PHP Brussels' => '//www.meetup.com/BrusselsPHP',
         'PHP Gent' => '//www.meetup.com/phpgent',
         'PHP Leuven' => '//www.meetup.com/PHP-Leuven-Web-Innovation-Group',
